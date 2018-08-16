91647d6c9b44667b43fd608de094ec40ebb0471f
qinzb <673565935@qq.com>
2018-08-16 18:10:21 +0800

    Machine Learning practice

--- tensorflow/tensorflow测试.py
+++ tensorflow/tensorflow测试.py
@@ -5,5 +5,21 @@ import numpy as np
 
 mnist = input_data.read_data_sets("C:/Users/Flyn/Desktop/MachineLearning/tensorflow/mnist")
 
-print(mnist.train.images.shape)
+X_train = tf.placeholder(tf.float32, [None, 784])
+y_train = tf.placeholder(tf.float32, [None, 1])
 
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(X_train, W) + b)
+
+cross_entropy = -tf.reduce_sum(y_train * tf.log(y))
+train = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train, feed_dict={X_train: batch_xs, y_train: batch_ys})
+        if i % 50 == 0:
+            print(sess.run(cross_entropy))
