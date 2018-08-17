4506bdbb912c946131ff5ba506de2236715e74af
qinzb <673565935@qq.com>
2018-08-17 11:31:06 +0800

    Machine Learning practice

--- tensorflow/tensorflow测试.py
+++ tensorflow/tensorflow测试.py
@@ -1,18 +1,16 @@
 import tensorflow as tf
 from mnist import input_data
-import matplotlib.pyplot as plt
-import numpy as np
 
-mnist = input_data.read_data_sets("C:/Users/Flyn/Desktop/MachineLearning/tensorflow/mnist")
+mnist = input_data.read_data_sets("./mnist", one_hot=True)
 
-X_train = tf.placeholder(tf.float32, [None, 784])
-y_train = tf.placeholder(tf.float32, [None, 1])
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
 
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
-y = tf.nn.softmax(tf.matmul(X_train, W) + b)
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-cross_entropy = -tf.reduce_sum(y_train * tf.log(y))
+cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 train = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 init = tf.global_variables_initializer()
@@ -20,6 +18,7 @@ with tf.Session() as sess:
     sess.run(init)
     for i in range(1000):
         batch_xs, batch_ys = mnist.train.next_batch(100)
-        sess.run(train, feed_dict={X_train: batch_xs, y_train: batch_ys})
-        if i % 50 == 0:
-            print(sess.run(cross_entropy))
+        sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
